5185f324344855a89c73b1193ba33524edb6975e
bleons36 <89231748+bleons36@users.noreply.github.com>
2023-03-09 20:52:53 +0900

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
-module "s3-bucket" {
+module "s3_bucket" {
   source  = "app.terraform.io/HashiCatforAWS/s3-bucket/aws"
-  version = "3.7.0"
-  
-   bucket_prefix = "sulee"
+  version = "2.8.0"
+
+  bucket_prefix = "sulee"
 }
